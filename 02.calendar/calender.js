5ba0348a41371567e9adc9c93f4a24f24ffb1959
ytakegawa <ytkgw1986@gmail.com>
2022-04-29 15:05:09 +0900

    CodeCheck: by ESlint
    ESlintによるコードチェック

--- 02.calendar/calender.js
+++ 02.calendar/calender.js
@@ -1,7 +1,7 @@
-let argv = require('minimist')(process.argv.slice(2))
+const argv = require('minimist')(process.argv.slice(2))
 const sprintf = require('sprintf-js').sprintf
 
-const offsetTime = 9 * 60 * 60 * 1000 //自分のPCだと実際の日本時間より9時間分遅れているためオフセット値を使う
+const offsetTime = 9 * 60 * 60 * 1000 //  自分のPCだと実際の日本時間より9時間分遅れているためオフセット値を使う
 const nowDate = new Date().getTime() + offsetTime
 const year = argv.y ? argv.y : new Date(nowDate).getFullYear()
 const month = argv.m ? argv.m - 1 : new Date(nowDate).getMonth()
@@ -14,16 +14,14 @@ const firstWeekSpace = space.repeat(new Date(firstDay).getDay())
 
 // ヘッダー表示
 console.log(`      ${year}.${month + 1}`)
-console.log("Su Mo Tu We Th Fr Sa")
+console.log('Su Mo Tu We Th Fr Sa')
 
 // ボディ表示
-days.forEach ((day, i) => {
+days.forEach((day, i) => {
   if (i === 0) process.stdout.write(firstWeekSpace)
   if (day.getDay() === 6) {
-    process.stdout.write(sprintf("%2d\n", day.getDate()))
-  }else{
-    process.stdout.write(sprintf("%2d ", day.getDate()))
+    process.stdout.write(sprintf('%2d\n', day.getDate()))
+  } else {
+    process.stdout.write(sprintf('%2d ', day.getDate()))
   }
 })
-
-
