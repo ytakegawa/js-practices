fc0e409750cd804c2c6f2d27dc72837d1aaf9c45
ytakegawa <ytkgw1986@gmail.com>
2022-05-10 15:12:01 +0900

    Refactoring
    重複部分のメソッド化(getJPDate)や変数処理の見直し

--- 02.calendar/calender.js
+++ 02.calendar/calender.js
@@ -2,26 +2,33 @@ const argv = require('minimist')(process.argv.slice(2))
 const sprintf = require('sprintf-js').sprintf
 
 const offsetTime = 9 * 60 * 60 * 1000 //  自分のPCだと実際の日本時間より9時間分遅れているためオフセット値を使う
-const nowDate = new Date().getTime() + offsetTime
-const year = argv.y ? argv.y : new Date(nowDate).getFullYear()
-const month = argv.m ? argv.m - 1 : new Date(nowDate).getMonth()
-const inputDate = new Date(year, month).getTime() + offsetTime
-const firstDay = new Date(inputDate).setDate(1)
-const lastDay = new Date(new Date(year, month + 1, 0).getTime() + offsetTime)
-const days = [...Array(lastDay.getDate())].map((_, i) => new Date(new Date(year, month, i + 1).getTime() + offsetTime))
+const year = argv.y || getJPDate().getFullYear()
+const month = argv.m - 1 || getJPDate().getMonth()
+const firstDay = getJPDate(year, month, 1)
+const lastDay = getJPDate(year, month + 1, 0)
+const days = [...Array(lastDay.getDate())].map((_, i) => getJPDate(year, month, i + 1))
 const space = '   '
-const firstWeekSpace = space.repeat(new Date(firstDay).getDay())
+const firstWeekSpace = space.repeat(new Date(firstDay).getDay());
+
+function getJPDate(year = 0, month = 0, day = 0) {
+  let date
+  if(year === 0 && month === 0 && day === 0){
+    date = new Date(new Date().getTime() + offsetTime)
+  }else{
+    date = new Date(new Date(year, month, day).getTime() + offsetTime)
+  }
+  return date
+}
 
 // ヘッダー表示
 console.log(`      ${year}.${month + 1}`)
 console.log('Su Mo Tu We Th Fr Sa')
 
 // ボディ表示
+process.stdout.write(firstWeekSpace)
 days.forEach((day, i) => {
-  if (i === 0) process.stdout.write(firstWeekSpace)
+  process.stdout.write(sprintf('%2d ', day.getDate()))
   if (day.getDay() === 6) {
-    process.stdout.write(sprintf('%2d\n', day.getDate()))
-  } else {
-    process.stdout.write(sprintf('%2d ', day.getDate()))
+    process.stdout.write(sprintf('\n'))
   }
 })
