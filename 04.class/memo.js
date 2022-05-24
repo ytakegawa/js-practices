9b32291b9915c988ef95d9357a105eebe655aa4c
ytakegawa <ytkgw1986@gmail.com>
2022-05-24 14:05:46 +0900

    Complete: implementation of requirements
    要件の実装完了

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,51 +1,31 @@
 const fs = require("fs")
 const enquirer = require('enquirer')
 const argv = require('minimist')(process.argv.slice(2))
-console.log(argv.l)
-
-
 
 class MemoAppExec{
   constructor(d, l, r){
-    this.d = d //argv.d = delete data option
-    this.l = l //argv.l = show title option
-    this.r = r //arvg.r = show body option
+    this.d = d  // argv.d = delete data option
+    this.l = l  // argv.l = show title option
+    this.r = r  // arvg.r = show body option
     this.memoFile = new MemoFile()
   }
-  run(){
-    if
-  }
-
-  showTiltle(){
-    this.memoFile.showTiltle()
+  main(){
+    if(this.d) return this.memoFile.showBody(this.d)
+    if(this.l) return this.memoFile.showTiltle()
+    if(this.r) return this.memoFile.showBody(this.d)
+    this.memoFile.addData()
   }
 }
-
-// function exec(showTitle = false, showBody =false, deleteData =false){
-//   console.log(showTitle, showBody, deleteData)
-
-// }
 
 class MemoFile {
   constructor(){
     this.memoFile = JSON.parse(fs.readFileSync('./memo_file.json', 'utf8'))
   }
-  // static loadFile(){
-  //   let readFile = JSON.parse(fs.readFileSync('./memo_file.json', 'utf8'))
-  //   return readFile
-  // }
+
   addData(){
-    // let memoParams = new MemoParams()
-    // let memoData = new MemoData(memoParams.generateId(), memoParams.generateBody())
-    // this.memoFile.memos.push(memoData.build_format())
-    // let formatData = {memos : this.memoFile.memos}
-    // let newMemoFileJSON = JSON.stringify(formatData)
     const memoData = new MemoData()
     this.memoFile.memos.push(memoData.build_format())
     MemoFile.saveData(this.memoFile.memos)
-    // const formatData = {memos : this.memoFile.memos}
-    // const newMemoFileJSON = JSON.stringify(formatData)
-    // fs.writeFileSync('./memo_file.json', newMemoFileJSON)
     console.log('メモデータが追加されました')
   }
 
@@ -74,7 +54,7 @@ class MemoFile {
       } else {
         console.log(result.body)
       }
-      }
+    }
 
     function choiceFormat(memos){
       let choices = []
@@ -89,76 +69,25 @@ class MemoFile {
   }
 
   static saveData(memoData){
-    // console.log(memoData)
     const formatData = {memos : memoData}
     const newMemoFileJSON = JSON.stringify(formatData)
     fs.writeFileSync('./memo_file.json', newMemoFileJSON)
   }
-
 }
 
 class MemoData extends MemoFile{
   constructor(){
     super()
-    // this.id = id
-    // this.body = body
     this.inputData = fs.readFileSync("/dev/stdin", "utf8").toString().split(' ')
   }
 
   build_format(){
     const id = this.memoFile.memos[0] ? (Math.max(...this.memoFile.memos.map((memo) => memo.id))) + 1 : 1
-    const dataFormat = {id: id.toString(), body: this.inputData.toString()}
+    const dataFormat = {id: id.toString(), body: this.inputData.join('\n').toString()}
     return dataFormat
   }
-
 }
 
-// class MemoParams {
-//   constructor(){
-//     this.inputData = fs.readFileSync("/dev/stdin", "utf8").toString().split(' ')
-//     this.memoFile = JSON.parse(fs.readFileSync('./memo_file.json', 'utf8'))
-//   }
-
-//   generateId(){
-//     return this.memoFile.memos[0] ? (Math.max(...this.memoFile.memos.map((memo) => memo.id))) + 1 : 1
-//   }
-
-//   generateBody(){
-//     let bodyData = this.inputData.toString()
-//     return bodyData
-//   }
-// }
-
-// class MemoParams extends MemoFile {
-//   constructor(){
-//     super()
-//     this.inputData = fs.readFileSync("/dev/stdin", "utf8").toString().split(' ')
-
-//   }
-
-//   generateId(){
-//     return this.memoFile.memos[0] ? (Math.max(...this.memoFile.memos.map((memo) => memo.id))) + 1 : 1
-//   }
-
-//   generateBody(){
-//     let bodyData = this.inputData.toString()
-//     return bodyData
-//   }
-// }
-
-
-// const memoParam = new MemoParams()
-// console.log(memoParam.generateId())
-// console.log(memoParam.generateBody())
-// const memoData = new MemoData()
-// console.log(memoData.build_format())
-// console.log(memoData.genmemoFile())
-// console.log(MemoData.generateId())
-
-// const newMemo = new MemoFile()
-// newMemo.addData()
-// const deleteFlag = true
-// newMemo.showBody(deleteFlag)
-// newMemo.showTiltle()
-
-exec(argv.l, argv.r, argv.d)
+// アプリの実行
+const memoApp = new MemoAppExec(argv.d, argv.l, argv.r)
+memoApp.main()
